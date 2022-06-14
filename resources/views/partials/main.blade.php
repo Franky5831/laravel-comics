332df52eebc7b3588b1632ff9b34cd69482599db
Franky5831 <fracianna5@gmail.com>
2022-06-14 16:19:09 +0200

    compleltato

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -5,10 +5,12 @@
 
     <div id="cardsContainer">
       <ul>
-          <li class="card-box">
-            <img class="card" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX">
-            <span class="text">Action Comics</span>
-          </li>
+        @foreach ($comic as $comics)
+        <li class="card-box">
+          <img class="card" src="{{$comics['thumb']}}" alt="{{$comics['type']}}">
+          <span class="text">{{$comics['series']}}</span>
+        </li>
+        @endforeach
       </ul>
     </div>
 </main>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comic = config('comics');
+    return view('home',['comic'=> $comic]);
+});
+
+Route::get('/shop', function () {
+    $comic = config('comics');
+    return view('shop',['comic' => $comic]);
 });
